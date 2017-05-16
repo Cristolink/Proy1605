5203f11a593891f992d0b1a39ee723bd4e07f0ce
cetecom <cetecom@LC08L5728>
2017-05-16 10:30:29 -0400

    el sugnod comitt pá

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<?php
+$_SESSION["saludo"]="Hola Mundo";
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+          <a href="revision.php">Revisi&oacute;n sesi&oacute;n</a>
+    </body>
+</html>
